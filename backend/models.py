11c58173183731c41a472ce55a49b90a36b20823
instep7 <ap.alowery@gmail.com>
2025-11-21 12:38:00 -0500

    Added gitignore

--- backend/models.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-class StockQuote(BaseModel):
-    symbol: str
-    current_price: float
-    high: float
-    low: float
-    open: float
-    previous_close: float
